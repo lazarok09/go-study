7618810aa78c04ee7fd20dfd37c9625b750c213d
lazarok09 <lazarok09@gmail.com>
2024-01-28 20:15:27 -0300

    feat(Funcoes): add functions

---
+++ Funcoes/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+// tipo atribuido pela última tipagem
+// Retorna dois tipos utilizand oos parênteses
+func calculoSomaESub(a, b int8) (int8, int8) {
+	var sub int8 = a - b
+	var sum int8 = a + b
+	return sum, sub
+}
+
+func main() {
+
+	var funcCallBack = func() (int8, int8) {
+		return calculoSomaESub(5, 8)
+	}
+
+	// Utiliza-se o underline para ocultar um valor
+	//  que você não deseja em uma função que retorna dois valores
+	var subtrair = func(a, b int8) int8 {
+		var _, sub int8 = calculoSomaESub(a, b)
+		return sub
+	}
+
+	fmt.Println(funcCallBack())
+	fmt.Println(subtrair(5, 2))
+}
