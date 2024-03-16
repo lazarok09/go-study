97fdc16c976206d7f905fb41c31291639f09fd06
lazarok09 <lazarok09@gmail.com>
2024-03-16 13:51:29 -0300

    refactor(Channels): change channel name to stream

---
+++ Goroutines/Waitgroup/go.mod
@@ -0,0 +1,3 @@
+module github.com/lazarok09/goroutines
+
+go 1.21.6
---
+++ Goroutines/Waitgroup/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/lazarok09/goroutines/waitgroup"
+)
+
+func main() {
+
+	waitgroup.WaitGroup()
+}
