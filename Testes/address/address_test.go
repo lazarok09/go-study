81e28589d7222fd302ece3568151bb0be6ef4321
lazarok09 <lazarok09@gmail.com>
2024-04-04 10:06:09 -0300

    feat(Testes): add golang more erro scenarios

--- Testes/address/address_test.go
+++ Testes/address/address_test.go
@@ -4,17 +4,31 @@ import (
 	"testing"
 )
 
+type Scenario struct {
+	input  string
+	output string
+}
+
 func TestAddressValidator(t *testing.T) {
+
 	results := Results{
 		valid:   "Valid address",
 		invalid: "Invalid address",
 	}
-
-	result := AddressValidator("New York Street")
-
-	expected := results.valid
-
-	if result != expected {
-		t.Errorf("An error ocurred when yor result was %s and you should receive %s ", result, expected)
+	scenarios := []Scenario{
+		{input: "New York Street", output: results.valid},
+		{input: "New York", output: results.invalid},
+		{input: "", output: results.invalid},
+		{input: "Foo Bar", output: results.invalid},
 	}
+	for _, scenario := range scenarios {
+		result := AddressValidator(scenario.input)
+
+		expected := scenario.output
+
+		if result != expected {
+			t.Errorf("An error ocurred when yor result was %s and you should receive %s ", result, expected)
+		}
+	}
+
 }
