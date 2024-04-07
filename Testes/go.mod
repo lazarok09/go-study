46f2092e5304157bfc8bdb03ca9b13d9b2d35cbd
lazarok09 <lazarok09@gmail.com>
2024-04-06 21:41:18 -0300

    feat(Testes): add address coverage file

---
+++ Testes/go.mod
@@ -0,0 +1,3 @@
+module lazarok09/testes
+
+go 1.21.6
---
+++ Testes/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"lazarok09/testes/address"
+)
+
+func main() {
+	// função para teste
+
+	address.AddressValidator("Olá mundo")
+}
