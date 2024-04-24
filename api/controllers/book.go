d8ec6e2057476a958f247df123df82a4dc991a50
lazarok09 <lazarok09@gmail.com>
2024-04-24 09:39:29 -0300

    feat(api): add get and create book

--- api/controllers/book.go
+++ api/controllers/book.go
@@ -2,29 +2,102 @@ package controllers
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/lazarok09/treinandosql/database"
 )
 
+type BookBody struct {
+	Name string `json:"name"`
+}
 type BookResponse struct {
-	Message string `json:"message"`
+	Response interface{} `json:"response"`
+}
+type Book struct {
+	Name string `json:"name"`
+	ID   int    `json:"id"`
 }
 
-func BookControler(w http.ResponseWriter, r *http.Request) {
+func CreateBook(w http.ResponseWriter, r *http.Request) {
 	connection, err := database.Connect()
 	if err != nil {
 		w.Write([]byte("An error ocurred when connecting the database"))
 
 	}
 	defer connection.Close()
-
-	message := BookResponse{Message: "Olá mundo"}
-
-	response, err := json.Marshal(message)
+	var book BookBody
+	requestBody, err := io.ReadAll(r.Body)
 	if err != nil {
+		w.Write([]byte("An error ocurred when reading the body database"))
+
+	}
+
+	if bookMarshallError := json.Unmarshal(requestBody, &book); bookMarshallError != nil {
 		w.Write([]byte("An error ocurred when parsing the message"))
 	}
 
-	w.Write(response)
+	resultOfInsertOperation, err := connection.Exec("INSERT INTO Book (name) VALUES (?)", book.Name)
+
+	if err != nil {
+		w.Write([]byte("An error ocurred when creating a book"))
+	}
+
+	bookIdResult, err := resultOfInsertOperation.LastInsertId()
+	if err != nil {
+		w.Write([]byte("An error ocurred when getting rows affected"))
+	}
+
+	if err != nil {
+		w.Write([]byte("An error ocurred when getting the final message"))
+
+	}
+	response := BookResponse{Response: bookIdResult}
+
+	finalResponse, err := json.Marshal(response)
+	if err != nil {
+		w.Write([]byte("An error ocurred when getting the final response"))
+
+	}
+
+	w.Write(finalResponse)
+}
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	connection, err := database.Connect()
+	if err != nil {
+		w.Write([]byte("An error ocurred when connecting the database"))
+
+	}
+	defer connection.Close()
+	rows, err := connection.Query("SELECT * FROM Book")
+
+	if err != nil {
+		w.Write([]byte("An error ocurred when scaning book"))
+
+	}
+	defer rows.Close()
+
+	var books []Book
+
+	for rows.Next() {
+		var book Book
+		if err := rows.Scan(
+			&book.ID,
+			&book.Name,
+		); err != nil {
+			w.Write([]byte("An error ocurred when scaning books"))
+
+		}
+		books = append(books, book)
+	}
+
+	response := BookResponse{Response: books}
+
+	finalResponse, err := json.Marshal(response)
+	if err != nil {
+		w.Write([]byte("An error ocurred when getting the final response"))
+
+	}
+
+	w.Write(finalResponse)
 }
