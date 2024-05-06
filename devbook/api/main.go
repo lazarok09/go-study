0ee6a0756ec5499d555af9106eabc6c36b8c5b9f
lazarok09 <lazarok09@gmail.com>
2024-05-06 20:31:48 -0300

    feat(config): add config package to load envs

--- devbook/api/main.go
+++ devbook/api/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config.LoadEnvs()
+
+	fmt.Println("Connection str at: %s", config.ConnectionStr)
 	r := router.Generate()
 
-	log.Fatal(http.ListenAndServe(":6000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
---
+++ devbook/api/src/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+// LOAD THE VARIABLES AS PUBLIC VALUES FROM LoadEnvs function
+var (
+	Port = 0
+	// the database connection string
+	ConnectionStr = ""
+)
+
+// Load the envs with a env package
+func LoadEnvs() {
+	var error error
+
+	if error = godotenv.Load(); error != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// Converting the port to 9000
+	Port, error = strconv.Atoi(os.Getenv("API_PORT"))
+	if error != nil {
+		Port = 9000
+	}
+
+	ConnectionStr = fmt.Sprintf("%s:%s@/%s?charset=utf8&paseTime=True&loc=Local", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_DATABASE"))
+
+}
