8a1d243ce666701fdda832430a9b54d8d4fe1517
lazarok09 <lazarok09@gmail.com>
2024-05-29 09:29:48 -0300

    feat(devbook/user): add user repository

--- devbook/api/src/controllers/users.go
+++ devbook/api/src/controllers/users.go
@@ -1,9 +1,40 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repository"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("MAYBE"))
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	var user models.User
+
+	if err = json.Unmarshal(body, &user); err != nil {
+		log.Fatal(err)
+	}
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	userRepository := repository.NewUsersRepository(db)
+
+	userId, err := userRepository.Create(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var userIdStr = fmt.Sprintf("%d", userId)
+
+	w.Write([]byte(userIdStr))
 
 }
 
---
+++ devbook/api/src/repository/user.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"api/src/models"
+	"database/sql"
+)
+
+// Represents the repository of users
+type users struct {
+	db *sql.DB
+}
+
+// Create a new user repository with the database.
+func NewUsersRepository(db *sql.DB) *users {
+	return &users{db}
+}
+
+// Create a user in the database and return his id if created
+func (repository users) Create(user models.User) (uint64, error) {
+	statment, err := repository.db.Prepare("INSERT INTO (name, nick, email, password) values(?, ?, ?, ?)")
+	if err != nil {
+		return 0, err
+	}
+	defer statment.Close()
+
+	result, err := statment.Exec(user.Name, user.Nick, user.Email, user.Password)
+
+	if err != nil {
+		return 0, err
+	}
+
+	lastInsertedID, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return uint64(lastInsertedID), nil
+}
